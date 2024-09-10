2bf82f8e9a8369b49cadcb921a69dde4ff4275b2
Frederico Dietz <fredericodietz@gmail.com>
2024-09-10 13:13:03 -0300

    test: only include relevant files to coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
-    include: ['src/**/test.{ts,tsx,js,jsx}', 'src/**/*.test.{ts,tsx,js,jsx}']
+    include: ['src/**/test.{ts,tsx,js,jsx}', 'src/**/*.test.{ts,tsx,js,jsx}'],
+    coverage: {
+      include: ['src/*/**/*.{ts,tsx,js,jsx}', 'src/*/**/*.test.{ts,tsx,js,jsx}']
+    }
   }
 });
